e3408960824131a77e5a2f00a66357f490d047e7
Monice Morais <monice.s.morais@gmail.com>
2023-11-14 23:22:35 -0300

    Adicionando dados

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,9 @@ function App() {
 
   const [products, setProducts] = useState([]);
 
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+
   // 1- Resgatando dados
   useEffect(() => {
     async function fetchData() {
@@ -20,6 +23,23 @@ function App() {
     fetchData();
   }, []);
 
+  // 2- Adicionando dados
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const product = {
+      name,
+      price
+    };
+
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(product)
+    });
+
+  }
 
   console.log(products);
 
@@ -27,10 +47,35 @@ function App() {
     <>
       <h1>Lista de produtos</h1>
       <ul>
-        {products.map((product)=>(
+        {products.map((product) => (
           <li key={product.id}>{product.name} - R$ {product.price}</li>
         ))}
       </ul>
+
+      <div className="add-product">
+        <form onSubmit={handleSubmit}>
+          <label>
+            Nome:
+            <input
+              type="text"
+              name="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </label>
+          <label>
+            Preço:
+            <input
+              type="number"
+              name="price"
+              value={price}
+              onChange={(e) => setPrice(e.target.value)}
+            />
+          </label>
+
+          <input type="submit" value="Criar" />
+        </form>
+      </div>
     </>
   )
 }
